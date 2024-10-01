9f19643b113e012bbeefcf557f5606d5c2b10caf
Cfelkins <Cfelkins@ucsc.edu>
2024-10-01 11:10:47 -0700

    Hello-World!

---
+++ Hello-World.py
@@ -0,0 +1,12 @@
+#this program will write Hello World!
+
+#this function tells the computer to print Hello World!
+def PrintHelloWorld():
+	print ("Hello World!")
+#This defines the main function of the program
+def main():
+	PrintHelloWorld()
+#When program is run, This executes first
+if __name__=="__main__":
+	main()
+	
