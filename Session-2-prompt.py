518d6cc92ede09a919e8b913d60b8abc5704b13a
Cfelkins <Cfelkins@ucsc.edu>
2024-10-02 11:27:34 -0700

    Session 2 Prompt
    Integers, floats, etc.

---
+++ Session-2-prompt.py
@@ -0,0 +1,26 @@
+##Define floating numbers
+
+a = 9.11
+b = 6.02
+
+#First, find the sum of floats
+
+s = a + b
+
+#Before printing the answer, let's print the data type of the answer. 
+
+print ('float:')
+print (s)
+
+##Define some integers and print the data type and then the difference. 
+
+x = 5
+y = 10
+
+print ('integer:')
+print (x - y)
+
+##Print data type of the product of an integer and a float and then print the product. 
+
+print ('float:')
+print (a * y)
