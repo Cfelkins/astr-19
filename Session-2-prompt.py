cd3faa8b3b8c0dc418712c8974b5c8627cd5a21b
Cfelkins <Cfelkins@ucsc.edu>
2024-10-03 11:37:57 -0700

    Session 2 Prompt

---
+++ Session-2-prompt.py
@@ -0,0 +1,34 @@
+def main():
+
+##Define floating numbers
+
+	a = 9.11
+	b = 6.02
+
+#First, find the sum of floats, s for sum.
+
+	s = a + b
+
+#Before printing the answer, let's print the data type of the answer. 
+
+	print (type(s),":")
+	print (s)
+
+##Define some integers and print the data type and then the difference. 
+
+	x = 5
+	y = 10
+
+	d = x - y  #d is the difference of x and y
+
+	print (type(d),':')
+	print (d)
+
+##Print data type of the product of an integer and a float and then print the product. 
+
+	p = a * y             #p for product
+	print (type(p),':')
+	print (p)
+
+if __name__ == '__main__':
+	main()
