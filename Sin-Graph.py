fd37255429d8f038663eadf8c15fb3284778836a
Cfelkins <Cfelkins@ucsc.edu>
2024-10-15 16:02:30 -0700

    Session 5 Prompt

---
+++ Sin-Graph.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+		#make x axis range between 0 and 2pi broken into 1000 points 
+	x = np.arange(0, 2*np.pi, .001)
+	#y is the sin of x
+	y = np.sin(x)
+
+	plt.plot(x,y)    #make a plot of this relationship
+	#label the axes
+	plt.xlabel('x')
+	plt.ylabel('sin(x)')
+	plt.show()            #show the graph on screen
+if __name__ == "__main__":
+	main()
