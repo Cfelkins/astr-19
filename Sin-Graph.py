9d72085864beee10626a1a31324b1f4072a479c5
Cfelkins <Cfelkins@ucsc.edu>
2024-10-15 16:03:27 -0700

    Sin-Graph.py

--- Sin-Graph.py
+++ Sin-Graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-		#make x axis range between 0 and 2pi broken into 1000 points 
+	#make x axis range between 0 and 2pi broken into 1000 points 
 	x = np.arange(0, 2*np.pi, .001)
 	#y is the sin of x
 	y = np.sin(x)
